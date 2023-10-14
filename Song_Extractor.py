fa24aad30f013f785134c607838a4ceac0e82c2a
Darshan Banait <112410078+DarshanBanait@users.noreply.github.com>
2023-10-14 17:17:17 +0530

    Song_Extractor.py

--- Song_Extractor.py
+++ Song_Extractor.py
@@ -2,16 +2,15 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # Spotify API credentials
-client_id = '63ead25c6796405385f2f8be8b937691'
-client_secret = '7d51fb1ec8b54081ada1f9e33348d628'
+client_id = 'Your spotify client_id'
+client_secret = 'Your spotify client_secret'
 redirect_uri = 'http://localhost:8888/callback'  # Set this as the redirect URI in your Spotify Developer application
 
 # Initialize the Spotify API client
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='playlist-read-private'))
 
 # Playlist ID of your "My Favorites" playlist
-playlist_id = '4LlehsGOThOGhW2mLQTYZd'
-# https://open.spotify.com/playlist/4LlehsGOThOGhW2mLQTYZd?si=a8904e8657d4451b
+playlist_id = 'Your spotify playlist_id'
 
 # Get the playlist
 playlist = sp.playlist(playlist_id, fields='tracks')
